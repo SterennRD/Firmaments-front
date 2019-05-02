e05e8758760dafaaadcc08743540c922256db724
SterennRD <s.reessedestoc@gmail.com>
2019-05-02 09:41:03 +0200

    Story modal

--- src/components/stories/StoryCard.js
+++ src/components/stories/StoryCard.js
@@ -5,11 +5,23 @@ const StoryCard = ({
         id, title, rating, author, description, categories, nb_likes, nb_favorites, nb_comments, status
     }) => {
     const category = categories.map(c => <span key={c.label}>{c.label}</span>)
+
+    const truncateText = (text, limit) => {
+        if (text.length > limit)
+            for (let i = limit; i > 0; i--){
+                if(text.charAt(i) === ' ' && (text.charAt(i-1) != ','||text.charAt(i-1) != '.'||text.charAt(i-1) != ';')) {
+                    return text.substring(0, i) + '...';
+                }
+            }
+        else
+            return text;
+    };
+
     return (
         <div key={id}>
             <h3><Link to={'/stories/see/' + id}>{title}</Link> <span className="badge badge-primary">{rating}</span></h3>
             <h4>Par <Link to={'/profile/' + author._id }>{author.username_display}</Link></h4>
-            <p>{description}</p>
+            <p>{truncateText(description, 50)}</p>
             <div>{category}</div>
             <div>{status}</div>
             <div>{nb_likes} <i className="fas fa-heart"></i> {nb_favorites} <i className="fas fa-star"></i> {nb_comments} <i className="fas fa-comment"></i></div>
--- src/components/stories/StoryModal.js
+++ src/components/stories/StoryModal.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
+import '../style/story-modal.scss';
+import {Link} from "react-router-dom";
+const ROOT_URL = window.location.href.indexOf('localhost') > 0 ? 'http://localhost:3000/images/uploads/' : '/images/uploads/';
 
 class StoryModal extends Component {
     render() {
         const {story} = this.props;
+        const category = story.category.map(c => <span key={c.label}>{c.label}</span>)
+        const cover = story.cover ? <img src={ROOT_URL + story.cover} className="w-25"/> : <div><i className="fas fa-book"></i> </div>;
         return (
-            <div>
-                Modal
-                {story.title}
-                <button onClick={this.props.hideModal}>Cacher</button>
+            <div className="storyModal">
+                <div className="storyModal__overlay" onClick={this.props.hideModal}></div>
+                <div className="storyModal__window">
+                    <div className="storyModal__cover">{cover}</div>
+                    <div className="storyModal__info">
+                        <div className="storyModal__stats">{story.nb_likes} <i className="fas fa-heart"></i> {story.nb_favorites} <i className="fas fa-star"></i> {story.nb_comments} <i className="fas fa-comment"></i></div>
+                        <h2>{story.title} <span className="badge badge-primary">{story.rating.label}</span></h2>
+                        <div>Par {story.author.username_display}</div>
+                        <div>{category}</div>
+                        <p>{story.description}</p>
+                        <div>
+                            <button>Lire</button>
+                            <button>Ajouter à une liste de lecture</button>
+                        </div>
+                    </div>
+                    <div className="storyModal__info_status">{story.status.label}</div>
+                </div>
             </div>
         );
     }
