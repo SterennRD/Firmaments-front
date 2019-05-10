631ecf628457cb79117314418eac8ef291c24e63
SterennRD <s.reessedestoc@gmail.com>
2019-05-10 13:59:10 +0200

    Notifications

--- src/components/notifications/Notification.js
+++ src/components/notifications/Notification.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 
 class Notification extends Component {
+    constructor(props) {
+        super(props);
+        this.handleRead = this.handleRead.bind(this)
+    }
+
+    handleRead(id) {
+
+    }
     render() {
         const {notifComment} = this.props.notifications;
         if (!notifComment.notif) {
@@ -11,7 +20,8 @@ class Notification extends Component {
             return (
                 <div>
                     <p>{notification.message}</p>
-                    {notification.user_from.username_display} a laissé un commentaire sur {notification.story}
+                    {notification.user_from.username_display} a laissé un commentaire sur <Link to={'/stories/see/' + notification.story_id._id}>{notification.story_id.title}</Link>
+                    <a id={notification._id} onClick={e => this.handleRead(e.target.id)}>Lu</a>
                 </div>
             )
         }
